0e8ca1cea17dffb794239aa9c274dc14978fde30
villageworks <villageworks@gmail.com>
2024-01-25 23:59:54 +0900

    ver.0.60: カテゴリー一覧画面表示

---
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Category;
+import com.example.demo.repository.CategoryRepository;
+
+
+@Controller
+public class CategoryController {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	// カテゴリー一覧画面表示
+	@GetMapping("/categories")
+	public String index(Model model) {
+		// すべてのカテゴリーをデータベースから取得
+		List<Category> categoryList = categoryRepository.findAll();
+		// 取得したカテゴリーリストをスコープに登録
+		model.addAttribute("categoryList", categoryList);
+		// 画面遷移
+		return "category/list";
+	}
+	
+}
---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+}
